b86e680d12357dd84144cddd66bf6feb18252c81
DaryaMarkova <Darya.Markova@dataart.com>
2019-09-17 12:57:53 +0300

    splitting webpack.config out

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,33 @@
 const webpack = require("webpack");
+const merge = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  devServer: {
+const serverConfig = require("./webpack.devserver.config");
+const commonConfig = merge([
+  {
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Webpack demo",
+      }),
+    ],
+  },
+]);
+
+const productionConfig = merge([]);
+
+const developmentConfig = merge([
+  serverConfig.devServer({
     host: process.env.HOST,
     port: process.env.PORT,
-    open: true,
-    overlay: true,
-    hotOnly: true,
-    // contentBase
-    // proxy
-    // headers
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Webpack demo 2",
-    }),
-    // new webpack.HotModuleReplacementPlugin()
-  ]
+  }),
+]);
+
+module.exports = mode => {
+  if (mode === "production") {
+    return merge(commonConfig, productionConfig, { mode });
+  }
+
+  return merge(commonConfig, developmentConfig, { mode });
 };
+
 // hot replacement with NodeJS server https://www.npmjs.com/package/webpack-hot-middleware
