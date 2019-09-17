3a834ea709c53753cef34a2273b0f8d6a1504030
DaryaMarkova <Darya.Markova@dataart.com>
2019-09-17 09:57:12 +0300

    devServer configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  devServer: {
+    host: process.env.HOST,
+    port: process.env.PORT,
+    open: true,
+    overlay: true
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: "Webpack demo",
